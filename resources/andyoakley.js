d44a39f6ec9d154bf2a77fd7186489c5bcdde909
Andy Oakley <andy@andyoakley.com>
2012-05-08 20:20:45 -0700

    blog support content

---
+++ resources/andyoakley.js
@@ -0,0 +1,14 @@
+// thank you! http://jmrware.com/articles/2010/linkifyurl/linkify.html
+var url_pattern = /(\()((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\))|(\[)((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\])|(\{)((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\})|(<|&(?:lt|#60|#x3c);)((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(>|&(?:gt|#62|#x3e);)|((?:^|[^=\s'"\]])\s*['"]?|[^=\s]\s+)(\b(?:ht|f)tps?:\/\/[a-z0-9\-._~!$'()*+,;=:\/?#[\]@%]+(?:(?!&(?:gt|#0*62|#x0*3e);|&(?:amp|apos|quot|#0*3[49]|#x0*2[27]);[.!&',:?;]?(?:[^a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]|$))&[a-z0-9\-._~!$'()*+,;=:\/?#[\]@%]*)*[a-z0-9\-_~$()*+=\/#[\]@%])/img;
+var url_replace = '$1$4$7$10$13<a href="$2$5$8$11$14">$2$5$8$11$14</a>$3$6$9$12';
+
+
+requestURL = "http://api.twitter.com/1/statuses/user_timeline/apoakley.json?count=5&include_rts=t&callback=?";
+$.getJSON(requestURL, function(json, status){
+	var content = "";
+	$.each(json, function(i){
+		tweet = this['text'].replace(url_pattern, url_replace);
+		content += "<li class=\"tweets\">" + tweet + "</li>";
+	});
+	$("ul#tweets").html(content);
+});
