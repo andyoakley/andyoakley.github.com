6668f8bf189fe9d6203b12a241d2f2e7b71e180e
Andy Oakley <andy@206industries.com>
2014-11-03 21:16:25 -0800

    Cleaning up css, updating look and feel

--- resources/andyoakley.js
+++ resources/andyoakley.js
@@ -3,12 +3,3 @@ var url_pattern = /(\()((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\)
 var url_replace = '$1$4$7$10$13<a href="$2$5$8$11$14">$2$5$8$11$14</a>$3$6$9$12';
 
 
-requestURL = "http://api.twitter.com/1/statuses/user_timeline/apoakley.json?count=5&include_rts=t&callback=?";
-$.getJSON(requestURL, function(json, status){
-	var content = "";
-	$.each(json, function(i){
-		tweet = this['text'].replace(url_pattern, url_replace);
-		content += "<li class=\"tweets\">" + tweet + "</li>";
-	});
-	$("ul#tweets").html(content);
-});
